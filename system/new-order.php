20ea75c956308f99619d8ebd6b73d4b6f08ff1fa
damboldor12 <maksimkashubatop@gmail.com>
2023-08-21 10:45:31 +0300

    reset

--- system/new-order.php
+++ system/new-order.php
@@ -3,7 +3,7 @@
 // Підключення до бази даних
 $servername = "localhost";
 $username = "amshrxqk_roof";
-$password = "********";
+$password = "1k2Ae!16iRGC;f";
 $dbname = "amshrxqk_roof";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
